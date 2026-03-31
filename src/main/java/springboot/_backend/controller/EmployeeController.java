b45e6069058e5461631699792776bf65b8b8f4bc
Vegh-dev <renanvegh@outlook.com>
2026-03-31 16:39:33 -0300

    Implementação do Update e Delete

--- src/main/java/springboot/_backend/controller/EmployeeController.java
+++ src/main/java/springboot/_backend/controller/EmployeeController.java
@@ -1,14 +1,14 @@
 package springboot._backend.controller;
 
 import lombok.AllArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springboot._backend.dto.EmployeeDto;
 import springboot._backend.service.EmployeeService;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -25,4 +25,34 @@ public class EmployeeController {
       return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
 
+    // Build Get Employee REST API
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId) {
+        EmployeeDto employeeDto = employeeService.getEmployeeByID(employeeId);
+        return ResponseEntity.ok(employeeDto);
+    }
+
+
+    // Build Get All Employees REST API
+    @GetMapping
+    public  ResponseEntity<List<EmployeeDto>> getAllEmployees(){
+        List<EmployeeDto> employees = employeeService.getAllEmployees();
+        return ResponseEntity.ok(employees);
+    }
+
+    //Build Update Employee REST API
+    @PutMapping("{id}")
+    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable ("id") Long employeeId,
+                                                      @RequestBody EmployeeDto updatedEmployee){
+       EmployeeDto employeeDto = employeeService.updateEmployee(employeeId,updatedEmployee);
+        return ResponseEntity.ok(employeeDto);
+    }
+
+
+    //Build Delete Employee REST API
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long employeeId){
+        employeeService.deleteEmployee(employeeId);
+        return ResponseEntity.ok("Funcionário Deletado com Sucesso!");
+    }
 }
--- src/main/java/springboot/_backend/service/EmployeeService.java
+++ src/main/java/springboot/_backend/service/EmployeeService.java
@@ -3,6 +3,19 @@ package springboot._backend.service;
 import springboot._backend.dto.EmployeeDto;
 import springboot._backend.entity.Employee;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeByID(long employeeID);
+
+    List<EmployeeDto> getAllEmployees();
+
+    EmployeeDto updateEmployee(Long employeeId, EmployeeDto updateEmployee);
+
+    void deleteEmployee(Long employeeId);
+
+
+
 }
